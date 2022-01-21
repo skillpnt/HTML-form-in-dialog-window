52db512c648a776594e04b4810bc4ef28537aa55
Roman Arnautov <arnautov.roma2000@gmail.com>
2022-01-21 18:12:50 +0700

    Initial commit

---
+++ handling_page.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+
+    Welcome <?php echo $_POST["firstname"];?> <br>
+    Your email address is: <?php echo $_POST["email"]; ?>
+    
+<a href="index.html">Back</a>
+</body>
+</html>
